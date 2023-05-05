57a1e708b995d89c2cca91effc9cb3b16a4cb5a0
Mat <malvin888@gmail.com>
2023-05-05 09:53:53 +0000

    initial

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,23 @@
 class CountElements
+    def initialize(elements)
+      @elements = elements
+    end
+  
+    def perform
+      count_elements
+    end
+  
+    private
+  
+    attr_reader :elements
+  
+    def count_elements
+      elements_count = Hash.new(0)
+      elements.each { |element| elements_count[element] += 1 }
+      elements_count
+    end
+  end
 
-end
+  output = CountElements.new([0,4,2,1,0,2,5]).perform
+  puts output
+  
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,41 @@
 class ValidatePassword
+    def initialize(password)
+      @password = password
+    end
+  
+    def perform
+      valid_length? && has_uppercase? && has_lowercase? && has_digit? && has_special_char? && max_two_consecutive_repeating_chars?
+    end
+  
+    private
+  
+    def valid_length?
+      @password.length >= 6 && @password.length <= 24
+    end
+  
+    def has_uppercase?
+      !(@password =~ /[A-Z]/).nil?
+    end
+  
+    def has_lowercase?
+      !(@password =~ /[a-z]/).nil?
+    end
+  
+    def has_digit?
+      !(@password =~ /\d/).nil?
+    end
+  
+    def has_special_char?
+      !(@password =~ /[!@#$%&*+=:;?<>]/).nil?
+    end
+  
+    def max_two_consecutive_repeating_chars?
+      @password !~ /(.)\1{2,}/
+    end
+  end
+  
 
-end
+output =  ValidatePassword.new("Password1!").perform
+puts output
+output = ValidatePassword.new("password1!").perform
+puts output
